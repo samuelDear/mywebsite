9a6e96482b8bbb7dfc9126b28d23d81e6637c79a
dearmaiker <samuel_dear@hotmail.com>
2020-05-20 17:48:26 -0400

    Creacion entrada de proyectos al menu

---
+++ src/api/projects/menuprojects.php
@@ -0,0 +1,37 @@
+<?php
+// app/api/login/login.php
+  header('Access-Control-Allow-Origin: *');
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+  header("Content-Type:application/json");
+  include("../dbcon.php");
+  include("../utils.php");
+
+  // parametros obligatorios
+  $parmsob = array("sessionid");
+  if (!parametrosValidos($_GET, $parmsob))
+      badEnd("400", array("msg"=>"Parametros obligatorios " . implode(", ", $parmsob)));
+
+  $out = new stdClass();
+  $sessionid = $_GET["sessionid"];
+
+  isSessionValid($db, $sessionid);
+
+  $sql = "SELECT count(id) AS qty FROM projects";
+  if (!$rs=$db->query($sql))
+    badEnd("500", array("msg"=>$db->error));
+
+  while($res = $rs->fetch_assoc()){
+    $record = new stdClass();
+    $record->name = "Proyectos";
+    $record->qty = $res["qty"];
+
+    $records [] = $record;
+  }
+
+  $out->records = $records;
+
+  header("HTTP/1.1 200");
+  echo (json_encode($out));
+  die();
+?>
