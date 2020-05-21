8ebf0de2df1c67b29958c5e224055deb68fc972b
dearmaiker <samuel_dear@hotmail.com>
2020-05-20 22:10:13 -0400

    Agrego de slidemenu

--- src/app/cms/project-edit/project-edit.component.ts
+++ src/app/cms/project-edit/project-edit.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../../services/projects.service';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-project-edit',
   templateUrl: './project-edit.component.html',
-  styles: []
+  styleUrls: ['./project-edit.component.scss']
 })
 export class ProjectEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private projectsServicio: ProjectsService,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
   }
 
+  logout(){
+    let user = {
+      sessionid: localStorage.sessionid,
+    }
+
+    this.loginService.logout(user).subscribe((res:any) => {
+      localStorage.clear();
+      this.router.navigateByUrl("/cms");
+    },
+    (error) => {
+      console.log(error);
+    })
+
+  }
+
+  navigate(url){
+    this.router.navigateByUrl("cms/" + url);
+  }
 }
