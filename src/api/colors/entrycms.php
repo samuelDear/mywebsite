4a657afefe08aa968c77b2772d86fff03789e27c
dearmaiker <samuel_dear@hotmail.com>
2020-05-21 18:38:38 -0400

    Creacion de servicio para obtener colores

---
+++ src/api/colors/entrycms.php
@@ -0,0 +1,45 @@
+<?php
+// app/api/login/login.php
+  header('Access-Control-Allow-Origin: *');
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+  header("Content-Type:application/json");
+  include("../dbcon.php");
+  include("../utils.php");
+
+  // parametros obligatorios
+  $parmsob = array("sessionid");
+  if (!parametrosValidos($_GET, $parmsob))
+      badEnd("400", array("msg"=>"Parametros obligatorios " . implode(", ", $parmsob)));
+
+  $out = new stdClass();
+  $sessionid = $_GET["sessionid"];
+
+  isSessionValid($db, $sessionid);
+
+  $sql = "SELECT * FROM colors ORDER BY id";
+  if (!$rs=$db->query($sql))
+      badEnd("500", array("msg"=>$db->error));
+
+  while ($row = $rs->fetch_assoc()) {
+    $record = new stdClass();
+    $record->id = (int)$row["id"];
+    $record->projectid = (int)$row["projectid"];
+
+    $sql = "SELECT name FROM projects WHERE id = " .$row["projectid"];
+    if (!$res=$db->query($sql))
+        badEnd("500", array("msg"=>$db->error));
+
+    $project = $res->fetch_assoc();
+
+    $record->nameproject = $project["name"];
+    $record->colorhex = $row["colorhex"];
+    $records [] = $record;
+  }
+
+  $out->records = $records;
+
+  header("HTTP/1.1 200");
+  echo (json_encode($out));
+  die();
+?>
