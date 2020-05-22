da894879eb812963568896af8caebd4d0551de82
dearmaiker <samuel_dear@hotmail.com>
2020-05-21 22:08:38 -0400

    Agrego de funcionalidad para mostrar datos del color

--- src/app/cms/colors-edit/colors-edit.component.ts
+++ src/app/cms/colors-edit/colors-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ColorsService } from '../../services/colors.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ProjectsService } from '../../services/projects.service';
 
 @Component({
   selector: 'app-colors-edit',
@@ -8,14 +10,35 @@ import { ColorsService } from '../../services/colors.service';
   styleUrls: ['./colors-edit.component.scss']
 })
 export class ColorsEditComponent implements OnInit {
-  color:any;
+  color: any;
+  projects: any;
+
+  editForm= this.fb.group({
+    id: [null, [Validators.required]],
+    colorhex: [null, [Validators.required]],
+    projectid: [null, [Validators.required]]
+  });
+
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private colorsService: ColorsService
+    private colorsService: ColorsService,
+    private projectsServicio: ProjectsService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    let user = {
+      sessionid: localStorage.sessionid,
+    }
+    this.projectsServicio.getProjectsCms(user).subscribe((result:any) => {
+      this.projects = result.records;
+      console.log(result.color);
+    },
+    (error) => {
+      console.log(error);
+    });
+
     this.activatedRoute.paramMap.subscribe(param => {
       let params = {
         id: param.get('id'),
@@ -27,11 +50,20 @@ export class ColorsEditComponent implements OnInit {
         this.colorsService.getColorById(params).subscribe((res: any) => {
           this.color = res.entry;
           console.log(this.color);
+          this.updateForm(this.color);
         },
         (error) => {
           console.log(error);
         });
       }
+    });
+  }
+
+  updateForm(color: any){
+    this.editForm.patchValue({
+      id: color.id,
+      colorhex: color.colorhex,
+      projectid: color.projectid
     });
   }
 
