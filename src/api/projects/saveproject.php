6ec56f33244d0cba6cb8c37d6fbc605819235ce1
dearmaiker <samuel_dear@hotmail.com>
2020-05-22 21:52:26 -0400

    Actualizacion de servicio para guardar archivos

--- src/api/projects/saveproject.php
+++ src/api/projects/saveproject.php
@@ -73,7 +73,32 @@
     "       VALUES ".insertFeatures($out->id, $_POST["tituloes"], $_POST["tituloen"], $_POST["dsces"], $_POST["dscen"]);
     if (!$db->query($sql))
       badEnd("500", array("msg"=>$db->error));
-      
+
+    $urlfolder = "../../assets/images/project-images/".$code;
+
+    delete_directory($urlfolder);
+    mkdir($urlfolder, 0777, true);
+
+    if(isset($_FILES["principalimg"])){
+      saveimg($_FILES["principalimg"],$urlfolder,"principalimg",$db,$out->id);
+    }
+
+    if(isset($_FILES["fmobileimg"])){
+      saveimg($_FILES["fmobileimg"],$urlfolder,"fmobileimg",$db,$out->id);
+    }
+
+    if(isset($_FILES["smobileimg"])){
+      saveimg($_FILES["smobileimg"],$urlfolder,"smobileimg",$db,$out->id);
+    }
+
+    if(isset($_FILES["laptopimg"])){
+      saveimg($_FILES["laptopimg"],$urlfolder,"laptopimg",$db,$out->id);
+    }
+
+    if(isset($_FILES["tabletimg"])){
+      saveimg($_FILES["tabletimg"],$urlfolder,"tabletimg",$db,$out->id);
+    }
+
   }else{
     $sql = "UPDATE projects SET".
     "       code = '".$code."', ".
@@ -103,6 +128,21 @@
   header("HTTP/1.1 200");
   echo (json_encode($out));
   die();
+
+  function saveimg($file,$url,$field,$db,$projectid){
+    //Recibimos el valor por el string que se envio en uploadids
+    $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+
+    //Movemos el FILE del directorio temporal al del sistema para su uso
+    if(move_uploaded_file($file["tmp_name"], $url . "/" . $field . "." . $ext)){
+      //Guardamos los datos del FILE en la BD para su busqueda
+      $sql = "UPDATE projects SET".
+      "      ".$field." = '".($url."/".$field.".".$ext)."'".
+      "      WHERE id = ". $projectid;
+      if (!$rs=$db->query($sql))
+        badEnd("500", array("msg"=>$db->error));
+    }
+  }
 
   function insertColors($colors, $projectid){
     $arrcolors = explode('~',$colors);
@@ -141,5 +181,25 @@
     }
 
     return substr($str, 0, -1);
+  }
+
+  function delete_directory($dirname) {
+    if (is_dir($dirname))
+      $dir_handle = opendir($dirname);
+
+    if (!$dir_handle)
+      return false;
+
+    while($file = readdir($dir_handle)) {
+      if ($file != "." && $file != "..") {
+        if (!is_dir($dirname."/".$file))
+          unlink($dirname."/".$file);
+        else
+          delete_directory($dirname.'/'.$file);
+        }
+    }
+    closedir($dir_handle);
+    rmdir($dirname);
+    return true;
   }
 ?>
