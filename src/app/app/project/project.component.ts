0e7cca3e10f684f2be906c3455662530014cb2af
dearmaiker <samuel_dear@hotmail.com>
2020-05-24 13:51:04 -0400

    Arreglo de estilos y navegacion a proyectos randoms

--- src/app/app/project/project.component.ts
+++ src/app/app/project/project.component.ts
@@ -15,13 +15,25 @@ export class ProjectComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     public translate: TranslateService,
-    private projectsServicio: ProjectsService
+    private projectsServicio: ProjectsService,
+    private router: Router
   ) {
     window.scroll(0,0);
   }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(params => {
+      window.scroll(0,0);
+      let links = document.getElementsByClassName('font-link');
+      for(let i = links.length - 1; i >= 0; i--){
+        links[i].remove();
+      }
+
+      let styles = document.getElementsByClassName('font-style');
+      for(let i = styles.length - 1; i >= 0; i--){
+        styles[i].remove();
+      }
+
       let nameProject = {
         code: params.get('id'),
       }
@@ -31,10 +43,12 @@ export class ProjectComponent implements OnInit {
         this.projectSelected = this.projectSelected.entry;
         console.log(this.projectSelected);
         for(let i = 0; i < this.projectSelected.typographies.length; i++){
-          this.setFont(this.projectSelected.typographies[i].url);
+
+          this.setFont(this.projectSelected.typographies[i].url, this.projectSelected.typographies[i].name);
 
           var style = document.createElement('style');
           style.type = 'text/css';
+          style.setAttribute('class',"font-style");
           style.innerHTML = `.${this.projectSelected.typographies[i].name}-s { font-family: '${this.projectSelected.typographies[i].name}'; }`;
           document.getElementsByTagName('head')[0].appendChild(style);
           setTimeout(() => {
@@ -45,11 +59,12 @@ export class ProjectComponent implements OnInit {
     });
   }
 
-  setFont(url: string){
+  setFont(url: string, name: string){
     var link = document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('type', 'text/css');
     link.setAttribute('href', url);
+    link.setAttribute('class',"font-link");
     document.head.appendChild(link);
   }
 
@@ -57,8 +72,10 @@ export class ProjectComponent implements OnInit {
     var style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = '.cssClass { color: #F00; }';
+  }
 
-
-
+  navigateUrl(code: string){
+    console.log(code);
+    this.router.navigate(['project',code]);
   }
 }
