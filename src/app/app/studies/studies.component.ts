2150c890e016bf36d9abe818960adeffa173f8ac
dearmaiker <samuel_dear@hotmail.com>
2020-05-31 22:48:09 -0400

    Agrego de estilos para linea de tiempo

--- src/app/app/studies/studies.component.ts
+++ src/app/app/studies/studies.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-studies',
   templateUrl: './studies.component.html',
-  styles: []
+  styleUrls: ['./studies.component.scss']
 })
 export class StudiesComponent implements OnInit {
   showButton: boolean = true;
-  
+  curses: any = [];
   constructor() { }
 
   ngOnInit() {
+    for(let i = 0; i < 10; i++){
+      this.curses.push({
+        title: 'Curso' + i,
+        datecreated: new Date(),
+        school: 'Instituto' + i + 1,
+      });
+    }
+    console.log(this.curses);
   }
 
 }
