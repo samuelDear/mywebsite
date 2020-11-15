e8f363545955f664040b216d725ac02c64af20d3
samuel <samuel_dear@hotmail.com>
2020-11-15 15:15:45 -0400

    Agrego de proyecto FJCINTRANET

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,30 +14,30 @@ export class HomeComponent implements OnInit {
     {
       name: 'FJC Intranet',
       es: {
-        resume: 'RESUMEN WEON'
+        resume: 'Es un sistema interno de pagina unica(SPA). Creado con Angular 8 y Java Spring Boot, para la gestion documental y de hallazgos en la Fundacion Jacinto Convit. Un sitio web que cuenta con estandares internacionales y bajo el estandar ISO 9001.'
       },
       en: {
-        resume: 'THIS IS A RESUME'
+        resume: 'It is an internal single page system (SPA). Created with Angular 8 and Java Spring Boot, for document and discovery management at the Fundacion Jacinto Convit. A website that has international standards and under the ISO 9001 standard.'
       },
       route: '/FJCIntranet'
     },
     {
       name: 'ORM',
       es: {
-        resume: 'RESUMEN WEON'
+        resume: 'Es una pagina web creada con HTML5, CSS3, JS y PHP. un sitio web responsivo con un diseño comodo para facilitar la experiencia del usuario y de un alto rendimiento. Creado para facilitar a los colegios de Venezuela la inscripcion a las Olimpiadas Recreativas Matematicas.'
       },
       en: {
-        resume: 'THIS IS A RESUME'
+        resume: 'It is a web page created with HTML5, CSS3, JS and PHP. a responsive website with a comfortable design to facilitate the user experience and high performance. Created to make it easier for schools in Venezuela to register for the Mathematical Recreation Olympics.'
       },
       route: '/ormv'
     },
     {
       name: 'My Portfolio',
       es: {
-        resume: 'RESUMEN WEON'
+        resume: 'Es un sitio web de pagina unica(SPA). Con un alto rendimiento, un sitio web responsivo que utiliza animaciones potentes. Tambien, usa el poder de los SVG para enriquecer la experiencia del usuario.'
       },
       en: {
-        resume: 'THIS IS A RESUME'
+        resume: "A website that is a Single-page application(SPA). With a high-performance, responsive website that uses powered animations. As also, uses the creative SVG's power for enriching user experience."
       },
       route: ''
     },
--- src/app/projects/fjcintranet/fjcintranet.component.ts
+++ src/app/projects/fjcintranet/fjcintranet.component.ts
@@ -7,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class FjcintranetComponent implements OnInit {
+  families = [{name: 'Oswald', link: `https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap`},
+  {name: 'Poppins', link: `https://fonts.googleapis.com/css2?family=Poppins&display=swap`}];
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.families.forEach((element, index) => {
+      console.log(element, index);
+      this.fontLoader(element);
+
+      if(document.getElementById(`typographyName${index+1}`) != undefined){
+        document.getElementById(`typographyName${index+1}`).innerHTML = element.name;
+        document.getElementById(`typographyName${index+1}`).style.fontFamily = element.name;
+      };
+
+      if(document.getElementById(`typographyExample${index+1}`) != undefined){
+        document.getElementById(`typographyExample${index+1}`).style.fontFamily = element.name;
+      };
+    });
   }
 
+  fontLoader = (param) => {
+    var headID = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+
+    headID.appendChild(link);
+
+    link.href = param.link;
+  }
 }
