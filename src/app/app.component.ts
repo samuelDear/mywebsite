5fd2c8633befaad2d12b1b2d502dc5b4d54f0c97
samueldear <samuel_dear@hotmail.com>
2020-12-04 12:27:46 -0400

    Add new transition in popstate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { Event as NavigationEvent } from "@angular/router";
+import { filter } from "rxjs/operators";
+import { NavigationStart } from "@angular/router";
+import { Router } from "@angular/router";
+import { navigationCustom } from './transitionPopstate';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +15,67 @@ export class AppComponent {
   buttonShow: boolean;
   loading: boolean = false;
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, router: Router) {
     translate.addLangs(['en','es']);
     translate.setDefaultLang('en');
 
     const browserLang = translate.getBrowserLang();
 
     translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
+
+    router.events
+			.pipe(
+				// The "events" stream contains all the navigation events. For this demo,
+				// though, we only care about the NavigationStart event as it contains
+				// information about what initiated the navigation sequence.
+				filter(
+					( event: NavigationEvent ) => {
+						return( event instanceof NavigationStart );
+					}
+				)
+			)
+			.subscribe(
+				( event: NavigationStart ) => {
+
+					console.group( "NavigationStart Event" );
+					// Every navigation sequence is given a unique ID. Even "popstate"
+					// navigations are really just "roll forward" navigations that get
+					// a new, unique ID.
+					console.log( "navigation id:", event.id );
+					console.log( "route:", event.url );
+					// The "navigationTrigger" will be one of:
+					// --
+					// - imperative (ie, user clicked a link).
+					// - popstate (ie, browser controlled change such as Back button).
+					// - hashchange
+					// --
+					// NOTE: I am not sure what triggers the "hashchange" type.
+          console.log( "trigger:", event.navigationTrigger );
+          
+          if(event.navigationTrigger == "popstate"){
+            navigationCustom( () => router.navigateByUrl(event.url) ); 
+          }
+					// This "restoredState" property is defined when the navigation
+					// event is triggered by a "popstate" event (ex, back / forward
+					// buttons). It will contain the ID of the earlier navigation event
+					// to which the browser is returning.
+					// --
+					// CAUTION: This ID may not be part of the current page rendering.
+					// This value is pulled out of the browser; and, may exist across
+					// page refreshes.
+					if ( event.restoredState ) {
+
+						console.warn(
+							"restoring navigation id:",
+							event.restoredState.navigationId
+						);
+
+					}
+
+					console.groupEnd();
+
+				}
+			);
   }
 
   showButton(show){
--- src/app/transitionPopstate.ts
+++ src/app/transitionPopstate.ts
@@ -5,42 +5,30 @@ export const navigationCustom = ( goto ) => {
     setTimeout(() => {
         endTx();
         goto();
-    }, 3000);
+    }, 10);
 }
 
 export const startTx = () => {
     document.body.style.overflow = 'hidden';
     document.body.style.pointerEvents = 'none';
     var div = document.createElement('div');
-    var name = document.createElement('p');
-    var hr = document.createElement('div');
 
     div.classList.add("loadScreen");
     div.classList.add('curtain');
-    div.style.backgroundImage = "url('assets/images/pat-dark.jpg')";
+    div.style.backgroundImage = '';
+    div.style.backgroundColor = '#000';
+    div.style.opacity = '1';
     div.setAttribute("id", "waitScreen");
 
-    name.innerHTML = 'Samuel Rojas';
-    name.classList.add('name');
-
-    hr.classList.add('loadingLine');
-
-    div.appendChild(name);
-    div.appendChild(hr);
-
     document.body.appendChild(div);
-    div.style.width = '0%';
-    setTimeout(() => {
-        document.getElementById("waitScreen").style.width = '100%';
-    }, 2);
+    document.getElementById("waitScreen").style.width = '100%';
 }
 
 export const endTx = () => {
-    document.getElementById("waitScreen").style.width = '0%';
+    document.getElementById("waitScreen").style.opacity = '0';
     setTimeout(() => {
         document.body.style.overflow = 'auto';
         document.body.style.pointerEvents = 'auto';
         document.body.removeChild(document.getElementById("waitScreen"));
-    }, 1200);
-    
+    }, 1200);   
 }
