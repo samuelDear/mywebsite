803b6488084dda790e4983f25c3924e1f6923530
SamuelDear <samuel_dear@hotmail.com>
2021-07-07 17:51:54 -0400

    Add afx trade project complete

--- src/app/projects/afxtrade/afxtrade.component.ts
+++ src/app/projects/afxtrade/afxtrade.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import { fontLoader } from '../../../main';
 
 @Component({
   selector: 'app-afxtrade',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./afxtrade.component.scss']
 })
 export class AfxtradeComponent implements OnInit {
+  families = [
+    {name: 'Poppins', link: `https://fonts.googleapis.com/css2?family=Poppins&display=swap`},
+    {name: 'Roboto', link: `https://fonts.googleapis.com/css2?family=Roboto&display=swap`}
+  ];
 
-  constructor() { }
+  constructor(
+    public translate: TranslateService,
+  ) { }
 
   ngOnInit(): void {
+    this.families.forEach((element, index) => {
+      //console.log(element, index);
+      fontLoader(element);
+      let div = document.getElementById(`typographyName${index+1}`);
+      if(div != undefined){
+        div.innerHTML = element.name;
+        div.style.fontFamily = element.name;
+      };
+
+      if(div != undefined){
+        div.style.fontFamily = element.name;
+      };
+    });
   }
 
 }
