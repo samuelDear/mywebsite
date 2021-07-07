b2bc3b34c54ca6712c233714e402441b27984f39
samueldear <samuel_dear@hotmail.com>
2021-07-07 14:00:09 -0400

    WIP AFX Trade high features

--- src/app/projects/afxtrade/afxtrade.component.ts
+++ src/app/projects/afxtrade/afxtrade.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-afxtrade',
   templateUrl: './afxtrade.component.html',
+  styleUrls: ['./afxtrade.component.scss']
 })
 export class AfxtradeComponent implements OnInit {
 
