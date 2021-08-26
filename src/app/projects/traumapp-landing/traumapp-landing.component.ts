30e22fdba1973ddea98f3059d4b94416c8b9f6d3
samuel <samuel_dear@hotmail.com>
2021-08-26 16:02:03 -0400

    Add related projects

--- src/app/projects/traumapp-landing/traumapp-landing.component.ts
+++ src/app/projects/traumapp-landing/traumapp-landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { fontLoader } from '../../../main';
-import { ColorType, Font } from '../../services/common';
+import { ColorType, FeatureType, Font, Project } from '../../services/common';
 
 @Component({
   selector: 'app-traumapp-landing',
@@ -12,6 +12,24 @@ export class TraumappLandingComponent implements OnInit {
   families: Font[] = [
     { name: 'Monserrat', link: `https://fonts.googleapis.com/css2?family=Montserrat&display=swap` },
     { name: 'Open Sans', link: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap` },
+  ];
+
+  ramdons: Project[] = [
+    {
+      code: '',
+      name: 'My Portafolio',
+      resume: 'myWeb.resume',
+    },
+    {
+      code: '/afxtrade',
+      name: 'AFX Trade',
+      resume: 'afxtrade.resume',
+    },
+    {
+      name: 'TraumApp',
+      code: '/traumapp',
+      resume: 'traumapp.resume',
+    },
   ];
 
   colors: ColorType[] = [
@@ -26,6 +44,13 @@ export class TraumappLandingComponent implements OnInit {
     },
     {
       dsc: '#64068D',
+    },
+  ];
+
+  features: FeatureType[] = [
+    {
+      title: 'afxtrade.featuretitle1',
+      dsc: 'afxtrade.feature1',
     },
   ];
 
