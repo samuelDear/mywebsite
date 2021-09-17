5e3c248788c5f973064814f72ee83619684c5f4d
SamuelDear <samuel_dear@hotmail.com>
2021-09-16 22:21:08 -0400

    Add resume section in component

---
+++ src/app/components/title-projects/title-projects.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { navigationCustom } from 'src/app/transition';
+
+@Component({
+  selector: 'app-title-projects',
+  templateUrl: './title-projects.component.html',
+})
+export class TitleProjectsComponent {
+  @Input() projectName = '';
+  @Input() dsc = '';
+  @Input() role = '';
+  @Input() agency = '';
+  @Input() date = '';
+  @Input() buttonType = '';
+  @Input() projectLink = '';
+
+  // eslint-disable-next-line
+  constructor(public router: Router) {}
+
+  navigateToHome(): void {
+    navigationCustom(() => this.router.navigate(['/']));
+  }
+}
