47a2f2c07c0189e1681bf3adc16f5f012a9bbc38
SamuelDear <samuel_dear@hotmail.com>
2021-09-29 10:59:19 -0400

    Add experiment section

--- src/app/services/common/index.ts
+++ src/app/services/common/index.ts
@@ -6,5 +6,17 @@ import { Course } from './courses';
 import { ShowHeader } from './showHeader';
 import { FeatureType } from './features';
 import { ColorType } from './colors';
+import { ExperimentType } from './experiments';
 
-export { emailForm, responseEmail, Font, Project, ProjectHome, Course, ShowHeader, FeatureType, ColorType };
+export {
+  emailForm,
+  responseEmail,
+  Font,
+  Project,
+  ProjectHome,
+  Course,
+  ShowHeader,
+  FeatureType,
+  ColorType,
+  ExperimentType,
+};
