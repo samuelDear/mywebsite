49a840cd42022830437583bcb6e1d7eedf840adc
SamuelDear <samuel_dear@hotmail.com>
2022-01-12 02:27:21 -0400

    [ADD] testing common translate

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ComponentsModule } from '../components/components.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, './assets/i18n/home/', '.json');
+export function createTranslateLoader(http: HttpClient) {
+  return new MultiTranslateHttpLoader(http, [
+    { prefix: './assets/i18n/home/', suffix: '.json' },
+    { prefix: './assets/i18n/common/copyright/', suffix: '.json' },
+  ]);
 }
 
 @NgModule({
