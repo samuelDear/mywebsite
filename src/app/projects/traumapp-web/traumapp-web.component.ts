27147a855d1397e4aaa2b6f802b80a619f2583fe
Samuel_dear <samuel_dear@hotmail.com>
2022-06-02 14:14:09 -0400

    Add initial details for traumapp page

--- src/app/projects/traumapp-web/traumapp-web.component.ts
+++ src/app/projects/traumapp-web/traumapp-web.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { I18Service } from 'src/app/services/i18n-service/i18n-service.service';
 
 @Component({
   selector: 'app-traumapp-web',
   templateUrl: './traumapp-web.component.html',
-  styleUrls: ['./traumapp-web.component.scss']
+  styleUrls: ['./traumapp-web.component.scss'],
 })
 export class TraumappWebComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public translate: TranslateService, private i18Service: I18Service) {
+    window.scroll(0, 0);
+    translate.use(i18Service.getLanguage());
   }
 
+  ngOnInit(): void {
+    this.i18Service.localeEvent.subscribe(locale => {
+      this.translate.use(locale);
+    });
+  }
 }
